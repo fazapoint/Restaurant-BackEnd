2c686d2296850686432ca075689da808b59027df
Anisykurli Faza Ramadhani <anisykurli.faza@ai.astra.co.id>
2025-08-24 22:03:40 +0700

    first commit

---
+++ Code/Restaurant.BackEnd/Restaurant.BackEnd.Service/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Get connection string from appsetting.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+// Register AppDbContext with Dependency Injection (DI)
+
+// Configure JSON serialization to handle reference loops
+
+// Add services to the container
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:3000",
+                "xxx.com") // React app URL
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Security middlewares
+app.UseHttpsRedirection(); // Force HTTP requests to redirect to HTTPS
+app.UseHsts(); // HTTP Strict Transport Security (HSTS)
+
+app.UseCors("AllowReactApp"); // Enable CORS here
+
+
+// line after app.Run() will never be executed
+app.Run();
+
